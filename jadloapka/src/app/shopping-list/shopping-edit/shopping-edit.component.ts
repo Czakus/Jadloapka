43216b2c307b40f27238650665540d7d1e0f5cd9
Czakus <szymon_krusik@PCZ-MP-1660.local>
2023-04-26 19:10:42 +0200

    Sekcja 6-89 Dodawanie ingredienta

--- jadloapka/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ jadloapka/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
   styleUrls: ['./shopping-edit.component.css']
 })
+
 export class ShoppingEditComponent {
+  @ViewChild('nameInput', {static: true}) nameInput: ElementRef;
+  @ViewChild('amountInput', {static: true}) amountInput: ElementRef;
+  @Output() addedIngredient = new EventEmitter<{ingredientName: string, ingredientAmount: number}>();
+
+  onAddedIngredient() {
+    this.addedIngredient.emit({
+      ingredientName: this.nameInput.nativeElement.value,
+      ingredientAmount: this.amountInput.nativeElement.value
+    })
+  }
 
 }
--- jadloapka/src/app/shopping-list/shopping-list.component.ts
+++ jadloapka/src/app/shopping-list/shopping-list.component.ts
@@ -7,9 +7,16 @@ import { Ingredient } from '../shared/ingredient.model';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent {
+
   ingredients: Ingredient[] = [
     new Ingredient('Apples', 5),
     new Ingredient('Tomatoes', 10)
   ];
 
+  ingredientWasAdded(ing: {ingredientName: string, ingredientAmount: number}) {
+    this.ingredients.push({
+      name: ing.ingredientName,
+      amount: ing.ingredientAmount
+    })
+  }
 }
