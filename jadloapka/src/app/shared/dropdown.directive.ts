f50f2f7d28e9d5c8271a6d4d0fd44262a03647b7
Czakus <krusik.szymon@gmail.com>
2023-05-09 23:54:55 +0200

    Sekcja 9.1 - Services

--- jadloapka/src/app/shared/dropdown.directive.ts
+++ jadloapka/src/app/shared/dropdown.directive.ts
@@ -1,16 +1,20 @@
-import { Directive, Input } from "@angular/core";
+import { parseHostBindings } from "@angular/compiler";
+import { Directive, ElementRef, HostBinding, HostListener} from "@angular/core";
 
 @Directive({
     selector: '[appDropdown]'
 })
 
 export class DropdownDirective {
-    @Input() dropdownClassValue: string;
-    @Input() set appDropdown(condition: boolean) {
-        if(condition) {
-            this.dropdownClassValue = "btn-group";
-        } else {
-            this.dropdownClassValue = "btn-group open";
-        }
+    // constructor(private elementRef: ElementRef) { }
+    // @HostListener('click') toggleOpen(eventData: Event) {
+    //     if(this.elementRef.nativeElement.className === "btn-group")
+    //         this.elementRef.nativeElement.className = "btn-group open"; else
+    //         this.elementRef.nativeElement.className = "btn-group";
+    // }
+
+    @HostBinding('class.open') isOpen = false;
+    @HostListener('click') toggleOpen(eventData: Event) {
+        this.isOpen = !this.isOpen;
     }
 }
