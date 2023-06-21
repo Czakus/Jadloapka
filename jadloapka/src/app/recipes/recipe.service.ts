8f5c64d6300ac0123da843341c7a99d0405661fd
Czakus <szymon_krusik@PCZ-MP-1660.local>
2023-06-21 19:13:33 +0200

    Sekcja 19/286 - Http request added to the app

--- jadloapka/src/app/recipes/recipe.service.ts
+++ jadloapka/src/app/recipes/recipe.service.ts
@@ -7,21 +7,23 @@ import { Ingredient } from "../shared/ingredient.model";
 
 export class RecipeService {
     recipesChanged = new Subject<Recipe[]>();
-    private recipes: Recipe[] = [
-        new Recipe( 'Tasty Schnitzel',
-                    'Super pyszny schnitzel - ROBI WRAZENIE',
-                    'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=556,505',
-                    [
-                        new Ingredient('Meat', 1),
-                        new Ingredient('French Fries', 20)
-                    ]),
-        new Recipe( 'DUZY TLUSTY BURGIR',
-                    'Dla Otylego Pana',
-                    'https://slevomat.sgcdn.cz/images/t/2000/14/75/14755452-ef79c0.jpg',
-                    [
-                        new Ingredient('Buns', 2),
-                        new Ingredient('Meat', 1)
-                    ])];
+    // private recipes: Recipe[] = [
+    //     new Recipe( 'Tasty Schnitzel',
+    //                 'Super pyszny schnitzel - ROBI WRAZENIE',
+    //                 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=556,505',
+    //                 [
+    //                     new Ingredient('Meat', 1),
+    //                     new Ingredient('French Fries', 20)
+    //                 ]),
+    //     new Recipe( 'DUZY TLUSTY BURGIR',
+    //                 'Dla Otylego Pana',
+    //                 'https://slevomat.sgcdn.cz/images/t/2000/14/75/14755452-ef79c0.jpg',
+    //                 [
+    //                     new Ingredient('Buns', 2),
+    //                     new Ingredient('Meat', 1)
+    //                 ])];
+
+    private recipes: Recipe[] = [];
 
     getRecipes() {
         return this.recipes.slice();
@@ -33,6 +35,11 @@ export class RecipeService {
 
     addRecipe(recipe: Recipe) {
         this.recipes.push(recipe);
+        this.recipesChanged.next(this.recipes.slice());
+    }  
+    
+    setRecipes(recipes: Recipe[]) {
+        this.recipes = recipes;
         this.recipesChanged.next(this.recipes.slice());
     }
 
