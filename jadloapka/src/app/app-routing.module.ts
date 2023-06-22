0c8f0ce06b127aa0efc7a4276419f5e1118a41b5
Czakus <szymon_krusik@PCZ-MP-1660.local>
2023-06-22 18:37:12 +0200

    Sekcja 20/289 - Auth Page Added

--- jadloapka/src/app/app-routing.module.ts
+++ jadloapka/src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { RecipeDetailComponent } from "./recipes/recipe-detail/recipe-detail.com
 import { RecipeStartComponent } from "./recipes/recipe-start/recipe-start.component";
 import { RecipeEditComponent } from "./recipes/recipe-edit/recipe-edit.component";
 import { RecipesResolverService } from "./recipes/recipes.resolver";
+import { AuthComponent } from "./auth/auth.component";
 
 const appRoutes: Routes = [
     {path: 'recipes', component: RecipesComponent, children: [
@@ -16,6 +17,7 @@ const appRoutes: Routes = [
         {path: ':id/edit', component: RecipeEditComponent, resolve:[RecipesResolverService]}
     ]},
     {path: 'shopping-list', component: ShoppingListComponent},
+    {path: 'auth', component: AuthComponent},
     {path: '', redirectTo: '/recipes', pathMatch: 'full'}
 ]
 
