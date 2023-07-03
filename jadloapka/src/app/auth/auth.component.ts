6095aefc15f3a0e5b91036e43a2b0ada52f99c2e
Czakus <szymon_krusik@PCZ-MP-1660.local>
2023-07-03 19:13:49 +0200

    Sekcja 20/300 - Rejestracja i Logowanie skonczone

--- jadloapka/src/app/auth/auth.component.ts
+++ jadloapka/src/app/auth/auth.component.ts
@@ -1,11 +1,48 @@
 import { Component } from "@angular/core";
+import { NgForm } from "@angular/forms";
+
+import { AuthResponseData, AuthService } from "./auth.service";
+import { Observable } from "rxjs";
 
 @Component({
     selector: 'app-auth',
     templateUrl: './auth.component.html',
-    styleUrls: ['./auth.component.css'] 
+    styleUrls: ['./auth.component.css']
 })
 
 export class AuthComponent {
+    constructor(private authService: AuthService) { }
+    isLoginMode = true;
+    isLoading = false;
+    error: string = null;
 
+    onSwitchMode() {
+        this.isLoginMode = !this.isLoginMode;
+        this.error = null;
+    }
+
+    onSubmit(form: NgForm) {
+        let authObs: Observable<AuthResponseData>;
+        this.error = null;
+        if (!form.valid) return;
+
+        const email = form.value.email;
+        const password = form.value.password;
+
+        this.isLoading = true;
+        if (!this.isLoginMode) {
+            authObs = this.authService.signUp(email, password);
+        } else {
+            authObs = this.authService.logIn(email, password);
+        }
+        form.reset();
+
+        authObs.subscribe(responseData => {
+            console.log(responseData);
+            this.isLoading = false;
+        }, errorMessage => {
+            this.error = 'An error during siginig up occured! <br>  Error message: ' + errorMessage;
+            this.isLoading = false;
+        })
+    }
 }
